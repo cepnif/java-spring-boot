1fb7b8169060f842ac1ffc7db2fd49ba2cbb911d
Fatih Cepni <fatih@fincityltd.com>
2020-12-22 22:02:23 +0000

    @Bean annotation added

--- 12-JavaCode-Configuration/src/main/java/com/fincityltd/configurations/MyAppConfig.java
+++ 12-JavaCode-Configuration/src/main/java/com/fincityltd/configurations/MyAppConfig.java
@@ -1,9 +1,22 @@
 package com.fincityltd.configurations;
 
+import com.fincityltd.services.Java;
+import com.fincityltd.services.Selenium;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.fincityltd")
 public class MyAppConfig {
+
+    @Bean
+    public Java java(){
+        return new Java();
+    }
+
+    @Bean
+    public Selenium selenium(){
+        return new Selenium();
+    }
 }
