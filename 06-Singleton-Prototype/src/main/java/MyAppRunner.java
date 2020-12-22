4fc427d30fac5fe22a47191f8b56686ecdd60da7
Fatih Cepni <fatih@fincityltd.com>
2020-12-22 15:46:30 +0000

    Bean scope implemented

--- 06-Singleton-Prototype/src/main/java/MyAppRunner.java
+++ 06-Singleton-Prototype/src/main/java/MyAppRunner.java
@@ -6,6 +6,11 @@ public class MyAppRunner {
     public static void main(String[] args) {
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
         Course course1 = container.getBean("java",Course.class);
-        course1.getTeachingHours();
+        Course course2 = container.getBean("java", Course.class);
+
+        System.out.println("Pointing to the same  object: " + (course1==course2));
+        System.out.println("Memory location for the course1 is: " + course1);
+        System.out.println("Memory location for the course2 is " + course2);
+
     }
 }
--- 06-Singleton-Prototype/src/main/java/com/fincityltd/services/Java.java
+++ 06-Singleton-Prototype/src/main/java/com/fincityltd/services/Java.java
@@ -5,9 +5,6 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Data
 public class Java implements Course {
 
     @Override
