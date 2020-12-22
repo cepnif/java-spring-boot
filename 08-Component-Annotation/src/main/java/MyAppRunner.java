06f70fbba49b5bf369e92baea5432af37615a05e
Fatih Cepni <fatih@fincityltd.com>
2020-12-22 16:36:03 +0000

    component annotation added

---
+++ 08-Component-Annotation/src/main/java/MyAppRunner.java
@@ -0,0 +1,12 @@
+import com.fincityltd.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyAppRunner {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course1 = container.getBean("java", Course.class);
+
+        course1.getTeachingHours();
+    }
+}
---
+++ 08-Component-Annotation/src/main/java/com/fincityltd/services/Java.java
@@ -0,0 +1,13 @@
+package com.fincityltd.services;
+
+import com.fincityltd.interfaces.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java implements Course {
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Java teaching hours: 20 ");
+    }
+}
