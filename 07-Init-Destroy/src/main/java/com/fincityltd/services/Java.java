7daa41ba922f92cdf1a3ac4acff8d90633331df2
Fatih Cepni <fatih@fincityltd.com>
2020-12-22 16:13:47 +0000

    close

--- 07-Init-Destroy/src/main/java/com/fincityltd/services/Java.java
+++ 07-Init-Destroy/src/main/java/com/fincityltd/services/Java.java
@@ -7,4 +7,12 @@ public class Java implements Course {
     public void getTeachingHours() {
         System.out.println("Java teaching hours: 23");
     }
+
+    public void myInitMethod(){
+        System.out.println("Init method executed");
+    }
+
+    public void myDestroyMethod(){
+        System.out.println("Destroy method executed");
+    }
 }
