ffbbc2d50f1f83a407abf523b82932d4bc8920cd
Fatih Cepni <fatih@fincityltd.com>
2020-12-22 17:24:01 +0000

    autowired annotation

--- 09-Autowired-Annotation/src/main/java/com/fincityltd/services/Java.java
+++ 09-Autowired-Annotation/src/main/java/com/fincityltd/services/Java.java
@@ -1,13 +1,21 @@
 package com.fincityltd.services;
 
 import com.fincityltd.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    private OfficeHours officeHours;
+
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
     @Override
     public void getTeachingHours() {
-        System.out.println("Java teaching hours: 20");
+        System.out.println("Java teaching hours: " + (20+ officeHours.getHours()));
     }
 }
