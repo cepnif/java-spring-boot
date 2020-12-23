f88eb32fc6cc45b9160059643de9170f428cb231
Fatih Cepni <fatih@fincityltd.com>
2020-12-23 10:07:06 +0000

    maintenance

--- 14-SpringBoot/src/main/java/com/fincityltd/Application.java
+++ 14-SpringBoot/src/main/java/com/fincityltd/Application.java
@@ -1,13 +1,18 @@
 package com.fincityltd;
 
+import com.fincityltd.interfaces.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+       ApplicationContext container = SpringApplication.run(Application.class, args);
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
     }
 
 }
