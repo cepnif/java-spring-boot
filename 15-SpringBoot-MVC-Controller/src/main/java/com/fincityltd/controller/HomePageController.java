8ccf0cde35b59598b7e0c967ff2742490386e414
Fatih Cepni <fatih@fincityltd.com>
2020-12-28 15:03:43 +0000

    get/post mapping, request param and path variable applied

--- 15-SpringBoot-MVC-Controller/src/main/java/com/fincityltd/controller/HomePageController.java
+++ 15-SpringBoot-MVC-Controller/src/main/java/com/fincityltd/controller/HomePageController.java
@@ -1,7 +1,7 @@
 package com.fincityltd.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomePageController {
@@ -11,13 +11,42 @@ public class HomePageController {
      return "homePage";
     }
 
-    @RequestMapping("/home")
-    public String getRequestMapping2(){
+    @GetMapping("/home")
+    public String getMapping1(){
         return "homePage";
     }
 
-    @RequestMapping("/main")
-    public String getRequestMapping3(){
+    @PostMapping("/main")
+    public String postMapping1(){
         return "homePage";
     }
+
+    @GetMapping("/home/{name}")
+    public String getMapping2 (@PathVariable("name") String name){
+
+        System.out.println("Path variable name is: " + name);
+        return "homePage";
+    }
+
+    @GetMapping("/home/{name}/{surname}")
+    public String getMapping3 (@PathVariable("name") String name, @PathVariable("surname") String surname){
+
+        System.out.println("Path variable name is: " + name);
+        System.out.println("Path variable surname is: " + surname);
+        return "homePage";
+    }
+
+    @GetMapping("/home/course")
+    public String getMapping4(@RequestParam("course") String course){
+        System.out.println("Request param is: " + course);
+        return "homePage";
+    }
+
+    @GetMapping("/course")
+    public String getMapping5(@RequestParam(value = "name", required = false, defaultValue = "FinCity") String name){
+        System.out.println("Request param is: " + name);
+        return "homePage";
+    }
+
+
 }
