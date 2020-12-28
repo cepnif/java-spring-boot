0c5a62a1d2e0eddf0e1f5e83d2ff43f20ca84eee
Fatih Cepni <fatih@fincityltd.com>
2020-12-28 15:29:37 +0000

    get/post mapping, request param and path variable applied

--- 15-SpringBoot-MVC-Controller/src/main/java/com/fincityltd/controller/HomePageController.java
+++ 15-SpringBoot-MVC-Controller/src/main/java/com/fincityltd/controller/HomePageController.java
@@ -8,29 +8,32 @@ public class HomePageController {
 
     @RequestMapping
     public String getRequestMapping1(){
+        System.out.println("#### getRequestMapping1 is executed ####");
      return "homePage";
     }
 
     @GetMapping("/home")
     public String getMapping1(){
+        System.out.println("#### getMapping1 is executed ####");
         return "homePage";
     }
 
     @PostMapping("/main")
     public String postMapping1(){
+        System.out.println("#### postMapping1 is executed ####");
         return "homePage";
     }
 
     @GetMapping("/home/{name}")
     public String getMapping2 (@PathVariable("name") String name){
-
+        System.out.println("#### getMapping2 is executed ####");
         System.out.println("Path variable name is: " + name);
         return "homePage";
     }
 
     @GetMapping("/home/{name}/{surname}")
     public String getMapping3 (@PathVariable("name") String name, @PathVariable("surname") String surname){
-
+        System.out.println("#### getMapping3 is executed ####");
         System.out.println("Path variable name is: " + name);
         System.out.println("Path variable surname is: " + surname);
         return "homePage";
@@ -38,12 +41,14 @@ public class HomePageController {
 
     @GetMapping("/home/course")
     public String getMapping4(@RequestParam("course") String course){
+        System.out.println("#### getMapping4 is executed ####");
         System.out.println("Request param is: " + course);
         return "homePage";
     }
 
     @GetMapping("/course")
     public String getMapping5(@RequestParam(value = "name", required = false, defaultValue = "FinCity") String name){
+        System.out.println("#### getMapping5 is executed ####");
         System.out.println("Request param is: " + name);
         return "homePage";
     }
