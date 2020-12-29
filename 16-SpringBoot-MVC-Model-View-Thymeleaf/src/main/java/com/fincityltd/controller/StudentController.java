a1c091feed86d827e9b128f16cffc129bf2a1fd3
Fatih Cepni <fatih@fincityltd.com>
2020-12-29 08:27:53 +0000

    java objects passed to UI

--- 16-SpringBoot-MVC-Model-View-Thymeleaf/src/main/java/com/fincityltd/controller/StudentController.java
+++ 16-SpringBoot-MVC-Model-View-Thymeleaf/src/main/java/com/fincityltd/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.fincityltd.controller;
 
+import com.fincityltd.model.Student;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Controller
@@ -12,8 +16,8 @@ import java.util.Random;
 public class StudentController {
 
     @GetMapping("/welcome")
-    public String homePage(Model model){
-        System.out.println("#### Student page homePage is executed ####");
+    public String homePage1(Model model){
+        System.out.println("#### Student page homePage1 is executed ####");
         String subject = "Streams";
         model.addAttribute("name","Software Testing");
         model.addAttribute("course","selenium");
@@ -22,6 +26,24 @@ public class StudentController {
 
         int studentId = new Random().nextInt(10000);
         model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(5);
+        model.addAttribute("numbers", numbers);
+
+        Student student = new Student(101,"Mitchel", "Smith");
+        model.addAttribute("student", student);
         return "student/welcome";
     }
+
+    @GetMapping("/register")
+    public String homePage2(Model model){
+        System.out.println("#### Student page homePage2 is executed ####");
+
+        return "student/register";
+    }
+
 }
