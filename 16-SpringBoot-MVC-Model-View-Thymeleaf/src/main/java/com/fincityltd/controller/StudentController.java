3c501ad756e9931b93de3c42e61b3ea68968c23c
Fatih Cepni <fatih@fincityltd.com>
2020-12-29 07:42:18 +0000

    student id added

--- 16-SpringBoot-MVC-Model-View-Thymeleaf/src/main/java/com/fincityltd/controller/StudentController.java
+++ 16-SpringBoot-MVC-Model-View-Thymeleaf/src/main/java/com/fincityltd/controller/StudentController.java
@@ -1,16 +1,27 @@
 package com.fincityltd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
     @GetMapping("/welcome")
-    public String homePage(){
+    public String homePage(Model model){
         System.out.println("#### Student page homePage is executed ####");
+        String subject = "Streams";
+        model.addAttribute("name","Software Testing");
+        model.addAttribute("course","selenium");
+        model.addAttribute("language","java");
+        model.addAttribute("subject",subject);
+
+        int studentId = new Random().nextInt(10000);
+        model.addAttribute("id",studentId);
         return "student/welcome";
     }
 }
