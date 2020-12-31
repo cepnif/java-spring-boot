e239b388432743b2a5ad443603f843a025a81e49
Fatih Cepni <fatih@fincityltd.com>
2020-12-31 01:22:30 +0000

    PostContruct added

--- 17-ORM-Table-Creation/src/main/java/com/fincityltd/OrmconfigApplication.java
+++ 17-ORM-Table-Creation/src/main/java/com/fincityltd/OrmconfigApplication.java
@@ -1,13 +1,30 @@
 package com.fincityltd;
 
+import com.fincityltd.entity.Car;
+import com.fincityltd.repository.CarRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class OrmconfigApplication {
 
+    @Autowired
+    CarRepo carRepo;
     public static void main(String[] args) {
         SpringApplication.run(OrmconfigApplication.class, args);
     }
 
+    @PostConstruct
+    public void dataInitializer(){
+        Car car1 = new Car("BMW","M5");
+        Car car2 = new Car("Toyota","Corolla");
+        Car car3 = new Car("Hyundai","Jazz");
+
+        carRepo.save(car1);
+        carRepo.save(car2);
+        carRepo.save(car3);
+    }
 }
