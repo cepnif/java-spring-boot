4c034d5730ed0480367a4a6e05c1c3d2b84a3573
Fatih Cepni <fatih@fincityltd.com>
2021-02-03 07:46:59 +0000

    Httpheaders implemented

--- 20-REST-Create-API/src/main/java/com/fincityltd/controller/ProductController.java
+++ 20-REST-Create-API/src/main/java/com/fincityltd/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.fincityltd.controller;
 
 import com.fincityltd.entity.Product;
 import com.fincityltd.service.ProductService;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +20,29 @@ public class ProductController {
     }
 
     @GetMapping(value = "/{id}")
-    public Product getProduct(@PathVariable("id") long id){
-        return productService.getProduct(id);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") long id){
+        return ResponseEntity.ok(productService.getProduct(id));
     }
 
     @GetMapping
-    public List<Product> getProducts (){
-        return productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts (){
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version", "fincityltd.v1");
+        responseHttpHeaders.set("Operation", "Get List");
+        return ResponseEntity
+                .ok()
+                .headers(responseHttpHeaders)
+                .body(productService.getProducts());
     }
 
     @PostMapping
-    public List<Product> createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity <List<Product>> createProduct(@RequestBody Product product){
+        List<Product> set = productService.createProduct(product);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "fincityltd.v1")
+                .header("Operation", "Create")
+                .body(set);
     }
 
     @DeleteMapping(value = "/{id}")
@@ -41,5 +54,4 @@ public class ProductController {
     public List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
         return productService.updateProduct(id, product);
     }
-
 }
