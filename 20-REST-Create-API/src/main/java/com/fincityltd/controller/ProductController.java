7bb9883e55c8d50e56602e9a1c29fae8ecc36b3e
Fatih Cepni <fatih@fincityltd.com>
2021-02-03 08:31:53 +0000

    Response wrapper implemented

--- 20-REST-Create-API/src/main/java/com/fincityltd/controller/ProductController.java
+++ 20-REST-Create-API/src/main/java/com/fincityltd/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.fincityltd.controller;
 
 import com.fincityltd.entity.Product;
+import com.fincityltd.entity.ResponseWrapper;
 import com.fincityltd.service.ProductService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,12 +49,37 @@ public class ProductController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public List<Product> deleteProduct(@PathVariable("id") long id){
-        return productService.delete(id);
+    public ResponseEntity <List<Product>> deleteProduct1(@PathVariable("id") long id){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Version","fincityltd.v1");
+        responseHeaders.set("Operation", "Delete");
+        List<Product> list = productService.delete(id);
+        return new ResponseEntity<>(list,responseHeaders,HttpStatus.OK);
     }
 
     @PutMapping(value = "/{id}")
-    public List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
-        return productService.updateProduct(id, product);
+    public ResponseEntity <List<Product>> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("Version", "fincityltd.v1");
+        map.add("Operation", "Update");
+        List<Product> list = productService.updateProduct(id, product);
+        return new ResponseEntity<>(list, map, HttpStatus.OK);
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<ResponseWrapper> readAllProducts(){
+        return ResponseEntity
+                .ok(new ResponseWrapper("Products successfully retrieved", productService.getProducts()));
+    }
+
+    @DeleteMapping("/delete1/{id}")
+    public ResponseEntity<ResponseWrapper> deleteProduct2(@PathVariable("id") long id){
+        return ResponseEntity
+                .ok(new ResponseWrapper("Product successfully deleted", productService.delete(id)));
+    }
+
+    @DeleteMapping("/delete2/{id}")
+    public ResponseEntity <ResponseWrapper> deleteProduct3(@PathVariable("id") long id){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ResponseWrapper("Product successfully deleted", productService.delete(id)));
     }
 }
